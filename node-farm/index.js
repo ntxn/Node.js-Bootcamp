90f702a19f18d17f1138661f9fc0f0d6e2e8c5a4
Kiera Nguyen <ngannguyen9017@gmail.com>
2020-04-08 14:42:52 -0700

    Read & Write files Asynchronously

--- node-farm/index.js
+++ node-farm/index.js
@@ -1,5 +1,6 @@
 const fs = require("fs");
 
+// Blocking synchorous way
 const textIn = fs.readFileSync("./txt/input.txt", "utf-8");
 console.log(textIn);
 
@@ -7,3 +8,21 @@ const textOut = `This is what we know about avocados: ${textIn}.\nCreated on ${D
 fs.writeFileSync("./txt/output.txt", textOut);
 
 console.log("File written");
+
+// Non-blocking asynchorous way
+fs.readFile("./txt/start.txt", "utf-8", (err, data1) => {
+  if (err) return console.log("ERROR!");
+
+  fs.readFile(`./txt/${data1}.txt`, "utf-8", (err, data2) => {
+    console.log(data2);
+
+    fs.readFile("./txt/append.txt", "utf-8", (err, data3) => {
+      console.log(data3);
+
+      fs.writeFile("./txt/final.txt", `${data2}\n${data3}`, "utf-8", (err) => {
+        console.log("Your file has been written");
+      });
+    });
+  });
+});
+console.log("Will read files!");
