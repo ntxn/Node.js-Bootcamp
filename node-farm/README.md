9b1deb280d4cbdb71fdd55441c7304a7d3672fec
Kiera Nguyen <ngannguyen9017@gmail.com>
2020-04-09 14:03:05 -0700

    Create a simple server using module http

--- node-farm/README.md
+++ node-farm/README.md
@@ -1,10 +1,19 @@
 Notes during this project
 
 - In node.js, use `require(...)` to import a module. Ex: `const fs = require('fs');` to import File System module
-- SYNCHRONOUS
+
+- <a href="https://github.com/ngannguyen117/Node.js-Bootcamp/commit/d67b8f1efa545b51dde4118c3b6293ec03b5a4b5">SYNCHRONOUS</a>
+
   - `fs.readFileSync` reading files synchoronously meaning before the program moves to the next line, it has to finish reading this file first
-  - Read & write files synchoronously using fs module <a href="https://github.com/ngannguyen117/Node.js-Bootcamp/commit/d67b8f1efa545b51dde4118c3b6293ec03b5a4b5">d67b8f1e</a>
+  - Read & write files synchoronously using fs module
   - Synchronous doesn't work well with Node.js because Node.js is single-threaded so if it waits for a line of code to be done before working on anything else, the rest of the code will be blocked and the program will be very slow.
-- ASYNCHORONOUS
+
+- <a href="https://github.com/ngannguyen117/Node.js-Bootcamp/commit/90f702a19f18d17f1138661f9fc0f0d6e2e8c5a4">ASYNCHORONOUS</a>
+
   - In asynchronous code, the heavy work of that single line will be done in the background while the rest of the code will still be running. Once it is done, the call back function that was registered before will be executed. This way, that line will not block anything
   - `fs.readFile` and `fs.writeFile` reads/writes files asynchronously
+
+- Simple Web Server
+  - `const http = require('http');` - module that gives networking capability such as building a http server by calling `http.createServer(....)`
+  - `createServer` takes a callback function that run everytime the server receive a request
+  - For the server to receive request, the server has to listen and wait for requests, i.e. `server.listen(...)`
--- node-farm/index.js
+++ node-farm/index.js
@@ -1,28 +1,40 @@
 const fs = require("fs");
+const http = require("http");
 
+/** -------------------------------------- FILES ----------------------------------------------- */
 // Blocking synchorous way
-const textIn = fs.readFileSync("./txt/input.txt", "utf-8");
-console.log(textIn);
+// const textIn = fs.readFileSync("./txt/input.txt", "utf-8");
+// console.log(textIn);
 
-const textOut = `This is what we know about avocados: ${textIn}.\nCreated on ${Date.now()}`;
-fs.writeFileSync("./txt/output.txt", textOut);
+// const textOut = `This is what we know about avocados: ${textIn}.\nCreated on ${Date.now()}`;
+// fs.writeFileSync("./txt/output.txt", textOut);
 
-console.log("File written");
+// console.log("File written");
 
 // Non-blocking asynchorous way
-fs.readFile("./txt/start.txt", "utf-8", (err, data1) => {
-  if (err) return console.log("ERROR!");
+// fs.readFile("./txt/start.txt", "utf-8", (err, data1) => {
+//   if (err) return console.log("ERROR!");
 
-  fs.readFile(`./txt/${data1}.txt`, "utf-8", (err, data2) => {
-    console.log(data2);
+//   fs.readFile(`./txt/${data1}.txt`, "utf-8", (err, data2) => {
+//     console.log(data2);
 
-    fs.readFile("./txt/append.txt", "utf-8", (err, data3) => {
-      console.log(data3);
+//     fs.readFile("./txt/append.txt", "utf-8", (err, data3) => {
+//       console.log(data3);
 
-      fs.writeFile("./txt/final.txt", `${data2}\n${data3}`, "utf-8", (err) => {
-        console.log("Your file has been written");
-      });
-    });
-  });
+//       fs.writeFile("./txt/final.txt", `${data2}\n${data3}`, "utf-8", (err) => {
+//         console.log("Your file has been written");
+//       });
+//     });
+//   });
+// });
+// console.log("Will read files!");
+
+/** -------------------------------------- SERVER ----------------------------------------------- */
+const server = http.createServer((req, res) => {
+  console.log(req);
+  res.end("Hello from the server!");
 });
-console.log("Will read files!");
+
+server.listen(8000, "127.0.0.1", () => {
+  console.log("Listing to requests on port 8000");
+}); // Listening for incoming request
