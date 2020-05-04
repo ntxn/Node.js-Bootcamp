8aff2b55469289445c21d100a8023d28858e1e02
Kiera Nguyen <ngannguyen9017@gmail.com>
2020-05-04 11:03:44 -0700

    Argument parsing with Yargs

--- note-taker/app.js
+++ note-taker/app.js
@@ -1,4 +1,55 @@
+const chalk = require('chalk');
+const yargs = require('yargs');
 const getNotes = require('./notes');
 
-const msg = getNotes();
-console.log(msg);
+// Add, remove, read, list
+
+// Create add command
+yargs.command({
+  command: 'add',
+  describe: 'Add a new note',
+  builder: {
+    title: {
+      describe: 'Note title',
+      demandOption: true,
+      type: 'string',
+    },
+    body: {
+      describe: 'Note body',
+      demandOption: true,
+      type: 'string',
+    },
+  },
+  handler: function (argv) {
+    console.log(`Title: ${argv.title}\nBody: ${argv.body}`);
+  },
+});
+
+// Create remove command
+yargs.command({
+  command: 'remove',
+  describe: 'Remove a note',
+  handler: function () {
+    console.log('Removing the note');
+  },
+});
+
+// Create list command
+yargs.command({
+  command: 'list',
+  describe: 'List your notes',
+  handler: function () {
+    console.log('Listing the notes');
+  },
+});
+
+// Create read command
+yargs.command({
+  command: 'read',
+  describe: 'Read a note',
+  handler: function () {
+    console.log('Reading the note');
+  },
+});
+
+yargs.parse();
