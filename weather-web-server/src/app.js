52d6110be24b54a060c403aafde2dfc727bf8240
Kiera Nguyen <ngannguyen9017@gmail.com>
2020-05-05 10:53:01 -0700

    Render dynamic pages with handlebars hbs view engine

--- weather-web-server/src/app.js
+++ weather-web-server/src/app.js
@@ -1,21 +1,42 @@
+const path = require('path');
 const express = require('express');
+const hbs = require('hbs');
 
 const app = express();
 
-app.get('', (req, res) => {
-  res.send('Hello express');
-});
+// Setup handlebars and views location
+app.set('view engine', 'hbs'); // at this step, the default view folder is called views at the root dir
+app.set('views', path.join(__dirname, '../templates/views')); // change the default view folder
+hbs.registerPartials(path.join(__dirname, '../templates/partials'));
 
-app.get('/help', (req, res) => {
-  res.send('Help page');
+// Setup static directory to serve
+app.use(express.static(path.join(__dirname, '../public')));
+
+// Setup routes
+app.get('', (req, res) => {
+  res.render('index', {
+    title: 'Weather app',
+    name: 'Robot',
+  });
 });
 
 app.get('/about', (req, res) => {
-  res.send('about page');
+  res.render('about', {
+    title: 'About Me',
+    name: 'Robot',
+  });
+});
+
+app.get('/help', (req, res) => {
+  res.render('help', {
+    helpText: 'This is a help message',
+    title: 'Help',
+    name: 'Robot',
+  });
 });
 
 app.get('/weather', (req, res) => {
-  res.send('weather page');
+  res.send({ location: 'Los Angeles', forecast: 'bla' });
 });
 
 app.listen(3000, () => {
