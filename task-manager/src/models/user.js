05b5174577a6eea347e9390abd100c2a7b402bf1
Kiera Nguyen <ngannguyen9017@gmail.com>
2020-05-06 15:21:28 -0700

    Task-manager: Hide private data

--- task-manager/src/models/user.js
+++ task-manager/src/models/user.js
@@ -57,6 +57,15 @@ userSchema.methods.generateAuthToken = async function () {
   return token;
 };
 
+userSchema.methods.toJSON = function () {
+  const user = this;
+  const userObject = user.toObject();
+  delete userObject.password;
+  delete userObject.tokens;
+  delete userObject.__v;
+  return userObject;
+};
+
 userSchema.statics.findByCredentials = async (email, password) => {
   const user = await User.findOne({ email });
   if (!user || !(await bcrypt.compare(password, user.password)))
