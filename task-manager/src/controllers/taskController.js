9506d32e25440d61270b79bc2a333e510fbe76eb
Kiera Nguyen <ngannguyen9017@gmail.com>
2020-05-06 14:42:10 -0700

    Task-manager: Refactor file structure

---
+++ task-manager/src/controllers/taskController.js
@@ -0,0 +1,68 @@
+const Task = require('../models/task');
+
+const getAllTasks = async (req, res) => {
+  try {
+    const tasks = await Task.find();
+    res.send(tasks);
+  } catch (err) {
+    res.status(500).send(err);
+  }
+};
+
+const createTask = async (req, res) => {
+  const task = new Task(req.body);
+  try {
+    await task.save();
+    res.status(201).send(task);
+  } catch (err) {
+    res.status(400).send(err);
+  }
+};
+
+const getTask = async (req, res) => {
+  try {
+    const task = await Task.findById(req.params.id);
+
+    if (!task) return res.status(404).send();
+    res.send(task);
+  } catch (err) {
+    res.status(500).send(err);
+  }
+};
+
+const updateTask = async (req, res) => {
+  const updateFields = Object.keys(req.body);
+  const allowedFields = ['description', 'completed'];
+  const areValidFields = updateFields.every((update) =>
+    allowedFields.includes(update)
+  );
+  if (!areValidFields)
+    return res.status(400).send({ error: 'Invalid update fields' });
+
+  try {
+    const task = await Task.findById(req.params.id);
+    if (!task) return res.status(404).send();
+
+    updateFields.forEach((field) => {
+      task[field] = req.body[field];
+    });
+
+    await task.save();
+    res.status(200).send(task);
+  } catch (err) {
+    res.status(400).send(err);
+  }
+};
+
+const deleteTask = async (req, res) => {
+  try {
+    const task = await Task.findByIdAndDelete(req.params.id);
+
+    if (!task) return res.status(404).send();
+    res.send(task);
+  } catch (err) {
+    res.status(500).send(err);
+  }
+};
+
+module.exports = { getAllTasks, createTask, getTask, updateTask, deleteTask };
---
+++ task-manager/src/controllers/userController.js
@@ -0,0 +1,90 @@
+const User = require('../models/user');
+
+const getAllUsers = async (req, res) => {
+  try {
+    const users = await User.find();
+    res.send(users);
+  } catch (err) {
+    res.status(500).send(err);
+  }
+};
+
+const createUser = async (req, res) => {
+  const user = new User(req.body);
+  try {
+    await user.save();
+    const token = await user.generateAuthToken();
+    res.status(201).send({ user, token });
+  } catch (err) {
+    res.status(400).send(err);
+  }
+};
+
+const getUser = async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+
+    if (!user) return res.status(404).send();
+    res.send(user);
+  } catch (err) {
+    res.status(500).send(err);
+  }
+};
+
+const updateUser = async (req, res) => {
+  const updateFields = Object.keys(req.body);
+  const allowedFields = ['name', 'email', 'password', 'age'];
+  const areValidFields = updateFields.every((update) =>
+    allowedFields.includes(update)
+  );
+  if (!areValidFields)
+    return res.status(400).send({ error: 'Invalid update fields' });
+
+  try {
+    const user = await User.findById(req.params.id);
+    if (!user) return res.status(404).send();
+
+    updateFields.forEach((field) => {
+      user[field] = req.body[field];
+    });
+
+    await user.save();
+    res.status(200).send(user);
+  } catch (err) {
+    res.status(400).send(err);
+  }
+};
+
+const deleteUser = async (req, res) => {
+  try {
+    const user = await User.findByIdAndDelete(req.params.id);
+
+    if (!user) return res.status(404).send();
+    res.send(user);
+  } catch (err) {
+    res.status(500).send(err);
+  }
+};
+
+const login = async (req, res) => {
+  try {
+    const user = await User.findByCredentials(
+      req.body.email,
+      req.body.password
+    );
+
+    const token = await user.generateAuthToken();
+    res.send({ user, token });
+  } catch (err) {
+    res.status(400).send();
+  }
+};
+
+module.exports = {
+  getAllUsers,
+  createUser,
+  getUser,
+  updateUser,
+  deleteUser,
+  login,
+};
